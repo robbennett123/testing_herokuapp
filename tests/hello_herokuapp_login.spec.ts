12b807635bed34d0668775d29b88c25e1826b49c
Rob Bennett <rob.contract@gmail.com>
2025-12-19 09:08:39 +0000

    deleted separate login file

--- tests/hello_herokuapp_login.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/');
-  await expect(page.locator('h1')).toContainText('Welcome to the-internet');
-
-});
